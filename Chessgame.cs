5e289644067aa2c98d225695cb71991d0e3893d7
Haekthor <hector.kolind@outlook.dk>
2021-09-14 12:07:10 +0200

    Add project files.

---
+++ Chessgame.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Stockfish.NET;
+
+
+namespace SkakRobot
+{
+    class Chessgame
+    {
+
+        public Chessgame()
+        {
+
+            Stockfish stockfish = new Stockfish("D:/Work_LOCAL/C# Workspace/ConsoleApp1/ConsoleApp1/Stockfish/win/stockfish_12_win_x64/stockfish_20090216_x64.exe");
+
+            stockfish.SetPosition("h2h3");
+            Console.WriteLine(stockfish.GetBoardVisual());
+            Console.WriteLine(stockfish.GetFenPosition());
+            Console.WriteLine(stockfish.IsMoveCorrect(""));
+            stockfish.SetPosition("h7h6");
+            Console.WriteLine(stockfish.GetBoardVisual());
+            Console.WriteLine(stockfish.GetFenPosition());
+            Console.WriteLine(stockfish.IsMoveCorrect("h7h6"));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using Stockfish.NET;
+using System;
+using System.Collections.Generic;
+
+namespace SkakRobot
+{
+    class Program
+    {
+        static Stockfish stockfish;
+        static String CurrentGame;
+        
+        static List<String> moves = new List<string>();
+
+        static void Main(string[] args)
+        {
+            init();
+
+            startGame();
+
+            ///Chessgame game = new Chessgame();
+
+        }
+
+        static void init()
+        {
+            stockfish = new Stockfish("D:/Work_LOCAL/C# Workspace/ConsoleApp1/ConsoleApp1/Stockfish/win/stockfish_12_win_x64/stockfish_20090216_x64.exe");
+        }
+
+
+        static void startGame()
+        {
+            ///CurrentGame = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+            stockFishTurn();
+        }
+
+        static void stockFishTurn()
+        {
+            ///stockfish.SetFenPosition(CurrentGame);
+            string move = stockfish.GetBestMove();
+            //Robot.move(move)
+
+            moves.Add(move);
+
+            stockfish.SetPosition(moves.ToArray());
+            ///CurrentGame = stockfish.GetFenPosition();
+            ///Console.WriteLine(CurrentGame);
+            Console.WriteLine(stockfish.GetBoardVisual());
+            stockfish.IsMoveCorrect("");
+            Console.WriteLine("Your move... Human");
+            playerTurn();
+
+        }
+
+        static void playerTurn()
+        {
+            string playerInput = Console.ReadLine();
+
+            ///stockfish.SetFenPosition(CurrentGame);
+            if (stockfish.IsMoveCorrect(playerInput))
+            {
+                moves.Add(playerInput);
+                stockfish.SetPosition(moves.ToArray());
+                ///CurrentGame = stockfish.GetFenPosition();
+                Console.WriteLine(stockfish.GetBoardVisual());
+                stockfish.IsMoveCorrect("");
+                stockFishTurn();
+            } else
+            {
+                Console.WriteLine("Bad move, try again");
+                playerTurn();
+            }
+        }
+    }
+}
